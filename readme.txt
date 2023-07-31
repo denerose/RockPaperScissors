21c23e2580150b3272a5e792f111b721814579f3
“Saphron” <saphron.hastie@gmail.com>
2023-07-31 14:57:46 +1000

    Initial Commit

---
+++ readme.txt
@@ -0,0 +1,2 @@
+Odin Project RPS project.
+Stage 1: basic game logic and random selection
---
+++ rps.js
@@ -0,0 +1,6 @@
+console.log("I am running!");
+
+function getComputerChoice() {
+    return "paper"
+}
+console.log(getComputerChoice)
