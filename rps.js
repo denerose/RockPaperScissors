a44574b490b4e5b39f96c14cb3dbd8b9c9092250
“Saphron” <saphron.hastie@gmail.com>
2023-08-04 12:49:16 +1000

    Takes player input

--- rps.js
+++ rps.js
@@ -6,6 +6,17 @@ function getComputerChoice() {
     return (options[(Math.floor(Math.random() * options.length))]);
 }
 
+function getPlayerChoice (){
+   let input = prompt("Choose: Rock, Paper or Scissors?", "Rock");
+   return toTitleCase(input);
+}
+
+function toTitleCase(str) {
+    return str.replace(/\w\S*/g, function(txt) {
+      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+  }
+
 function playRound(playerSelection, computerSelection) {
     console.log("Player chooses..." + playerSelection)
     console.log("Computer chooses..." + computerSelection)
@@ -16,14 +27,24 @@ function playRound(playerSelection, computerSelection) {
         result = "Rock breaks scissors. PLAYER wins!";
      } else if (playerSelection=="Rock" && computerSelection=="Paper"){
         result = "Paper smothers rock. Computer wins!"
+     } else if (playerSelection=="Paper" && computerSelection=="Scissors") {
+        result = "Scissors cut paper. Computer wins!";
+     } else if (playerSelection=="Paper" && computerSelection=="Rock"){
+        result = "Paper smothers rock. PLAYER wins!"
+     } else if (playerSelection=="Scissors" && computerSelection=="Paper") {
+        result = "Scissors cut paper. PLAYER wins!";
+     } else if (playerSelection=="Scissors" && computerSelection=="Rock"){
+        result = "Rock breaks scissors. Computer wins!"
+     } else if (playerSelection=="Bite"){
+        result = "You cannot bite the computer."
      } else {
-        result = "I am a confused computer! Help me!"; 
+        result = `I am a confused computer! I don't recognise ${playerSelection}. Help me?`; 
       } 
       return (result)
       console.log(result) 
   }
    
-  const playerSelection = prompt("Rock, paper, or scissors?", "Rock");
+  const playerSelection = getPlayerChoice();
   const computerSelection = getComputerChoice();
 
   console.log(playRound(playerSelection, computerSelection));
