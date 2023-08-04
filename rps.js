5bf081cf2393f23df3a2ebf6e03dae445c64ee0e
“Saphron” <saphron.hastie@gmail.com>
2023-08-04 11:32:49 +1000

    Remove switch. Add If else for results.

--- rps.js
+++ rps.js
@@ -8,10 +8,24 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
     console.log("Player chooses..." + playerSelection)
-    console.log("Computer chooses..." + computerSelection)    
+    console.log("Computer chooses..." + computerSelection)
+    let result = "TBC"
+    if(playerSelection==computerSelection) {
+        result = "DRAW! No one wins!";
+     } else if (playerSelection=="Rock" && computerSelection=="Scissors") {
+        result = "Rock breaks scissors. PLAYER wins!";
+     } else if (playerSelection=="Rock" && computerSelection=="Paper"){
+        result = "Paper smothers rock. Computer wins!"
+     } else {
+        result = "I am a confused computer! Help me!"; 
+      } 
+      return (result)
+      console.log(result) 
   }
    
-  const playerSelection = "rock";
+  const playerSelection = options[0];
   const computerSelection = getComputerChoice();
 
   console.log(playRound(playerSelection, computerSelection));
+
+  
